1fff89e386ccc54e418ad24d3598c8b3650c827c
D0LNA <thanadol.thongrit@mail.kmutt.ac.th>
2022-11-02 22:02:59 +0700

    Add CategoryItem

--- lib/selectcategory.dart
+++ lib/selectcategory.dart
@@ -1,30 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_3/components/header_bar.dart';
+import './components/header_bar.dart';
+import './components/CategoryItem.dart';
 
 class selectcategory extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-        title: Center(
-            child: Container(
-          child: Text('Equaler',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Color.fromRGBO(50, 48, 45, 1),
-              )),
-          width: 378,
-        )),
-        actions: [
-          Transform.scale(
-              scale: 1,
-              child: IconButton(
-                icon: Image.asset('asset/logo/logoapp.png'),
-                iconSize: 30,
-                onPressed: () {},
-              ))
-        ],
-        iconTheme: IconThemeData(color: Color.fromRGBO(50, 48, 45, 1)),
-        backgroundColor: Color.fromRGBO(246, 240, 235, 1));
+    return Scaffold(
+      appBar: Headerbar(
+        headerTitle: 'Select Category',
+      ),
+      body: GridView(
+          padding: EdgeInsets.all(25),
+          children: [
+            CategoryItem(),
+            CategoryItem(),
+            CategoryItem(),
+            CategoryItem()
+          ],
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+          )),
+    );
   }
 
   @override

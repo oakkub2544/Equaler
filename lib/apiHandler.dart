4683842a4ef3cdf4a4b41243f8bbd50e7f673a64
Sundione <tawan.tps@mail.kmutt.ac.th>
2022-11-21 17:20:22 +0700

    comment api key

--- lib/apiHandler.dart
+++ lib/apiHandler.dart
@@ -18,6 +18,8 @@ class apiHandler {
   }
 
   static Future<dynamic> getNews(List<String> parameter) async {
+    //สำรอง: pub_13699438c8b63ce784777146929c925c17dbd
+    //จริง: pub_124249313445c3671fd4d175e97415511a437
     String rawUrl =
         "https://newsdata.io/api/1/news?apikey=pub_13699438c8b63ce784777146929c925c17dbd";
     for (var par in parameter) {

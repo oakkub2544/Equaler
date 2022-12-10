c1e3afee099627df9cae4cbaf2c656f0be4e79bd
TSurasee <75519998+TSurasee@users.noreply.github.com>
2022-12-10 23:37:42 +0700

    Update news_card_list.dart

--- lib/components/news_card_list.dart
+++ lib/components/news_card_list.dart
@@ -6,6 +6,8 @@ import './api_error_message.dart';
 import './empty_news_list_message.dart';
 
 class NewsCardList extends StatelessWidget {
+//========================= Initial Constructor ================================
+
   final Future<Map> newsData;
   final double listHeight;
   final bool isBigCard;
@@ -13,24 +15,27 @@ class NewsCardList extends StatelessWidget {
       {required this.newsData,
       required this.listHeight,
       required this.isBigCard});
-
+//==============================================================================
+  
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: listHeight,
       width: MediaQuery.of(context).size.width * 0.9,
       child: FutureBuilder<dynamic>(
-        future: newsData,
+        future: newsData, //Set newsData to future type
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return LoadingIndicator();
           } else if (snapshot.hasData && snapshot.data['results'].length != 0) {
+            //If api has data display Big news card
             return isBigCard
                 ? ListView.builder(
-                    scrollDirection: Axis.horizontal,
+                    scrollDirection: Axis.horizontal,// Scroll direction of ListView is vertical
                     itemCount: snapshot.data['results'].length,
                     itemBuilder: (context, index) {
-                      return BigNewsCard(
+                      return BigNewsCard(//Display Big news card
+                          //Send constructor to BigNewsCard
                           imgUrl: snapshot.data['results'][index]['image_url'],
                           newsTitle: snapshot.data['results'][index]['title'],
                           newsDate: snapshot.data['results'][index]['pubDate'],
@@ -42,7 +47,8 @@ class NewsCardList extends StatelessWidget {
                 : ListView.builder(
                     itemCount: snapshot.data['results'].length,
                     itemBuilder: (context, index) {
-                      return NewsCard(
+                      return NewsCard(//Display News card
+                          //Send constructor to NewsCard
                           imgUrl: snapshot.data['results'][index]['image_url'],
                           newsTitle: snapshot.data['results'][index]['title'],
                           newsDate: snapshot.data['results'][index]['pubDate'],
@@ -51,9 +57,9 @@ class NewsCardList extends StatelessWidget {
                           newsDesc: snapshot.data['results'][index]
                               ['description']);
                     });
-          } else if (!snapshot.hasData) {
+          } else if (!snapshot.hasData) {//api has no data display error
             return ApiErrorMessage();
-          } else if (snapshot.data['results'].length == 0) {
+          } else if (snapshot.data['results'].length == 0) {//data from the api has a length 0 display no Popular News
             return EmptyNewsListMessage("No News Right Now");
           }
           // By default, show a loading spinner.

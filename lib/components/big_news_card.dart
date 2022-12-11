462afc9f8e5527573ad1f37757a3a10d21a02a61
TSurasee <75519998+TSurasee@users.noreply.github.com>
2022-12-11 20:26:18 +0700

    Update big_news_card.dart

--- lib/components/big_news_card.dart
+++ lib/components/big_news_card.dart
@@ -37,13 +37,12 @@ class BigNewsCard extends StatelessWidget {
         )
       },
       child: Container(
-        //big news card formatting
+        //Create and formate Big news card
         width: MediaQuery.of(context).size.width * 0.9,
         height: MediaQuery.of(context).size.height * 0.21,
         margin: const EdgeInsets.symmetric(vertical: 5),
         //responsive design from screen
         child: Card(
-          //Create Big news card
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(20.0),
           ),

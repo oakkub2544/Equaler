3cd291846a60418e8777f2ae66c2798375f87587
kanawat <91202429+oakkub2544@users.noreply.github.com>
2022-12-11 21:59:49 +0700

    Update header_bar.dart

--- lib/components/header_bar.dart
+++ lib/components/header_bar.dart
@@ -11,28 +11,34 @@ class Headerbar extends StatelessWidget with PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-        title: SizedBox(
-            width: 378,
-            child: Text(headerTitle,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  color: Color.fromRGBO(50, 48, 45, 1),
-                ))),
-        actions: [
-          Transform.scale(
-              scale: 1,
-              child: IconButton(
-                icon: Image.asset('asset/logo/logoapp.png'), //Logo app
-                iconSize: 30,
-                onPressed: () { //When you pressed on the logo will go to the main page
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => MyApp())); //Go to main page
-                }, 
-              )),
-        ],
-        iconTheme: const IconThemeData(color: Color.fromRGBO(50, 48, 45, 1)),
-        backgroundColor: const Color.fromRGBO(246, 240, 235, 1));
+    return Semantics(
+      label: "Menu Bar",
+      child: AppBar(
+          title: SizedBox(
+              width: 378,
+              child: Text(headerTitle,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: Color.fromRGBO(50, 48, 45, 1),
+                  ))),
+          actions: [
+            Transform.scale(
+                scale: 1,
+                child: Semantics(
+                  label: "Back to Home Page",
+                  child: IconButton(
+                    icon: Image.asset('asset/logo/logoapp.png'),
+                    iconSize: 30,
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => MyApp()));
+                    }, //Pressed icon to go main page
+                  ),
+                )),
+          ],
+          iconTheme: const IconThemeData(color: Color.fromRGBO(50, 48, 45, 1)),
+          backgroundColor: const Color.fromRGBO(246, 240, 235, 1)),
+    );
   }
 
   @override
